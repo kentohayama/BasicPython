24e5fe16de48f0552d82c00235e401a75937065f
kentohayama <kent20210325@keio.jp>
2023-11-29 19:57:45 +0900

    Python入門回答間違えて別のブランチでも編集

--- pi.py
+++ pi.py
@@ -3,5 +3,13 @@ text = """
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-# TODO
+# テキストから単語の文字数を取得してリスト化
+word_lengths = list(map(len, text.split()))
 
+# リストの要素を文字列に結合
+result = ''.join(map(str, word_lengths))
+
+print(result)
+
+# 以下をTERMINALに入力し確認
+# C:/Users/kent2/AppData/Local/Microsoft/WindowsApps/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/python.exe c:/Users/kent2/OneDrive/デスクトップ/PG/BasicPython/pi.py
